eee017c306f236cf0c11fb805915f388526a9adb
xeyc0 <marioche@students.zhaw.ch>
2021-04-18 09:09:26 +0200

    commit W2 by cm

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Week2
 
 17.04.2021/Onur: Lege neues Gradle Projekt an
+18.04.2021/Cheyco: Pull von GitRepository OnurFalcone
--- app/src/main/java/Week2/App.java
+++ app/src/main/java/Week2/App.java
@@ -5,13 +5,41 @@ package Week2;
 
 import com.google.common.net.InternetDomainName;
 import com.mitchtalmadge.asciidata.graph.ASCIIGraph;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 public class App {
+
+    public static final int ART_SIZE_SMALL = 10;
+    public static final int ART_SIZE_MEDIUM = 18;
+    public static final int ART_SIZE_LARGE = 24;
+    public static final int ART_SIZE_HUGE = 32;
+ 
+    private static final String DEFAULT_ART_SYMBOL = "*";
+ 
+    public enum ASCIIArtFont {
+        ART_FONT_DIALOG("Dialog"), ART_FONT_DIALOG_INPUT("DialogInput"), 
+        ART_FONT_MONO("Monospaced"),ART_FONT_SERIF("Serif"), ART_FONT_SANS_SERIF("SansSerif");
+ 
+        private String value;
+ 
+        public String getValue() {
+            return value;
+        }
+ 
+        private ASCIIArtFont(String value) {
+            this.value = value;
+        }
+    }
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello Week 2!";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println(new App().getGreeting());
 
         InternetDomainName owner =
@@ -21,8 +49,88 @@ public class App {
         System.out.println(owner.toString());
         System.out.println("valid:" + valid);
 
-
+        //onur
         String graph = ASCIIGraph.fromSeries(new double[] { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0,}).plot();  
         System.out.println(graph);
+
+        //cheyco
+        App artGen = new App();
+         
+        System.out.println();
+        artGen.printTextArt("Hello", App.ART_SIZE_MEDIUM, ASCIIArtFont.ART_FONT_MONO,"-");
+        System.out.println();
+         //cheyco end
+       
+         //colin
+
+        
+    }
+
+    /**
+     * Prints ASCII art for the specified text. For size, you can use predefined sizes or a custom size.
+     * Usage - printTextArt("Hi",30,ASCIIArtFont.ART_FONT_SERIF,"@");
+     * @param artText
+     * @param textHeight - Use a predefined size or a custom type
+     * @param fontType - Use one of the available fonts
+     * @param artSymbol - Specify the character for printing the ascii art
+     * @throws Exception
+     */
+    private void printTextArt(String artText, int textHeight, ASCIIArtFont fontType, String artSymbol) throws Exception {
+        String fontName = fontType.getValue();
+        int imageWidth = findImageWidth(textHeight, artText, fontName);
+ 
+        BufferedImage image = new BufferedImage(imageWidth, textHeight, BufferedImage.TYPE_INT_RGB);
+        Graphics g = image.getGraphics();
+        Font font = new Font(fontName, Font.BOLD, textHeight);
+        g.setFont(font);
+ 
+        Graphics2D graphics = (Graphics2D) g;
+        graphics.drawString(artText, 0, getBaselinePosition(g, font));
+ 
+        for (int y = 0; y < textHeight; y++) {
+            StringBuilder sb = new StringBuilder();
+            for (int x = 0; x < imageWidth; x++)
+                sb.append(image.getRGB(x, y) == Color.WHITE.getRGB() ? artSymbol : " ");
+            if (sb.toString().trim().isEmpty())
+                continue;
+            System.out.println(sb);
+        }
+    }
+ 
+    /**
+     * Convenience method for printing ascii text art.
+     * Font default - Dialog,  Art symbol default - *
+     * @param artText
+     * @param textHeight
+     * @throws Exception
+     */
+    private void printTextArt(String artText, int textHeight) throws Exception {
+        printTextArt(artText, textHeight, ASCIIArtFont.ART_FONT_DIALOG, DEFAULT_ART_SYMBOL);
+    }
+ 
+    /**
+     * Using the Current font and current art text find the width of the full image
+     * @param textHeight
+     * @param artText
+     * @param fontName
+     * @return
+     */
+    private int findImageWidth(int textHeight, String artText, String fontName) {
+        BufferedImage im = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
+        Graphics g = im.getGraphics();
+        g.setFont(new Font(fontName, Font.BOLD, textHeight));
+        return g.getFontMetrics().stringWidth(artText);
+    }
+ 
+    /**
+     * Find where the text baseline should be drawn so that the characters are within image
+     * @param g
+     * @param font
+     * @return
+     */
+    private int getBaselinePosition(Graphics g, Font font) {
+        FontMetrics metrics = g.getFontMetrics(font);
+        int y = metrics.getAscent() - metrics.getDescent();
+        return y;
     }
 }
